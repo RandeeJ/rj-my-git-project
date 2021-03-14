365183d5abd6edacac5e49ef6e6b969415136dde
Randee Johnson <randeejohnson@Randees-MacBook-Pro.local>
2021-03-14 12:13:51 -0400

    PLease work

---
+++ my_file.py
@@ -0,0 +1,27 @@
+mkdir "folder name"
+cd "folder name"
+touch "file name"
+git init
+ls #to see all files
+ls -la #to see all hidden files
+git status
+git add "file name (python file)"
+git status #to see updated addition
+git commit -m "Initial commit"
+git status
+#on github, create new repository and copy second set of codes, then run
+#should now see local file on github
+
+#------------
+
+CREATING A README FILE
+
+touch README.md #md means markdown
+git status
+git add .
+git status
+git commit -m "Add 'filename' file to repository"
+git push
+
+#-------------
+
